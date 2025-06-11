bc3240fe4bee59c171aefc55ea2575edd333dfca
Arul <arulprakashr02@gmail.com>
2025-06-11 12:02:36 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 # Example file with test tokens (DO NOT USE REAL TOKENS!)
 # These are fake tokens for testing purposes
 # Added this comment to trigger a new scan
-API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"  # GitHub token
+API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"  # GitHub token
 AWS_ACCESS = "AKIAIOSFODNN7EXAMPLE"  # AWS access key
 SECRET_KEY = """-----BEGIN RSA PRIVATE KEY-----
 MIIEpQIBAAKCAQEA1234567890
