1db70f80dfdb31411404176c28e259975d688cb8
Arul <arulprakashr02@gmail.com>
2025-06-11 10:48:59 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,15 @@
+# Example file with test tokens (DO NOT USE REAL TOKENS!)
+# These are fake tokens for testing purposes
+# Added this comment to trigger a new scan
+API_KEY = "ghp_1234567890abcdefghijklmnopqrstuvwxyz"  # GitHub token
+AWS_ACCESS = "AKIAIOSFODNN7EXAMPLE"  # AWS access key
+SECRET_KEY = """-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA1234567890
+-----END RSA PRIVATE KEY-----"""
+# Some regular code
+def main():
+    print("Hello World!")
+    print("Testing token scanner...")  # Added this line
+if __name__ == "__main__":
+    main()
+
